b86a6df8a64105ee6ab26f5ba984444846ddd3bf
Likogeles <57628609+Likogeles@users.noreply.github.com>
2024-01-16 04:10:09 +0400

    Добавлено меню "о программе" и исправлены мелкие баги

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 from flask import Flask, request, redirect
 import re
 
@@ -6,6 +8,8 @@ from UserList import UserList
 
 app = Flask(__name__)
 userList = UserList()
+max_password_tries = 3
+password_tries = 0
 
 pattern = r'(?:\d[^\d]+)*\d(?:[^\d]+\d)*'
 
@@ -14,6 +18,12 @@ userList.add_user(User("1", "1@ya.ru", "1", False, True, 0, 30))
 userList.add_user(User("12", "12@ya.ru", "12", False, True, 0, 30))
 userList.add_user(User("123", "123@ya.ru", "123", False, True, 0, 30))
 userList.add_user(User("1234", "1234@ya.ru", "1234", False, True, 0, 30))
+
+# <meta charset="utf-8">
+# <meta name="viewport" content="width=device-width, initial-scale=1">
+# <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+# <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 
 
 def head_html(title):
@@ -27,6 +37,30 @@ def head_html(title):
             """
 
 
+def nav_html():
+    return """<nav class="navbar navbar-expand-lg navbar-light bg-light">
+              <a class="navbar-brand" href="#">Лабораторная работа ИБ №1</a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+            
+              <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <div class="dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Справка
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <a class="dropdown-item" href="/about">О программе</a>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            </nav>"""
+
+
 @app.route("/")
 @app.route("/home")
 def index():
@@ -37,14 +71,80 @@ def index():
                     <div class="container mt-5">
                         <h1>Система учёта пользователей</h1>
                         <br>
-                        <a class="btn btn-primary" href="log_in">Войти</a>
+                        <a class="btn btn-primary" href="/log_in">Войти</a>
+                        <br>
+                        <br>
+                        <a class="btn btn-warning" href="/about">Справка</a>
                     </div>
                 </body>
             </html>"""
 
 
+@app.route("/about")
+def about():
+    return f"""<!DOCTYPE html>
+            <html>
+                {head_html("О программе")}
+                <body>
+                    <div class="container mt-5">
+                        <h1>О программе</h1>
+                        <br>
+                        <h3>Система учёта пользователей</h3>
+                        <br>
+                        <h4>Автор: Долгов Кирилл Сергеевич (ПИбд-42)</h4>
+                        <br>
+                        <h4>Вариант - 11</h4>
+                        <br>
+                        <h4>Ограничения на выбираемые пароли: чередование цифр, знаков препинания и снова цифр.</h4>
+                        <br>
+                        <h4>Используемый режим шифрования алгоритма DES для шифрования файла: CFB.</h4>
+                        <br>
+                        <h4>Добавление к ключу случайного значения: Да.</h4>
+                        <br>
+                        <h4>Используемый алгоритм хеширования пароля: MD5.</h4>
+                        <br>
+                        <br>
+                        <a class="btn btn-primary" href="javascript:history.back()">Назад</a>
+                    </div>
+                </body>
+            </html>"""
+
+
+@app.route("/exit")
+def exitApp():
+    return redirect("/")
+
+
+@app.route("/drop")
+def drop():
+    sys.exit()
+
+
+@app.route("/change_is_password_limited")
+def change_is_password_limited():
+    user_login = request.args.get("user")
+    if user_login:
+        user = userList.get_user_by_login(user_login)
+        if user:
+            user.set_is_password_limited(not user.get_is_password_limited())
+
+    return redirect("/admin")
+
+
+@app.route("/change_is_blocked")
+def change_is_blocked():
+    user_login = request.args.get("user")
+    if user_login:
+        user = userList.get_user_by_login(user_login)
+        if user:
+            user.set_is_blocked(not user.get_is_blocked())
+
+    return redirect("/admin")
+
+
 @app.route("/log_in", methods=["POST", "GET"])
 def log_in():
+    global password_tries
     login_errors = []
     if request.method == "POST":
         login_form = request.form['login_form']
@@ -58,7 +158,10 @@ def log_in():
         else:
             user = userList.get_user_by_login(login_form)
             if user.get_password() != User.hash_password(password_form):
-                login_errors.append("Неверный пароль")
+                password_tries += 1
+                login_errors.append(f"Неверный пароль. Осталось попыток: {max_password_tries-password_tries}")
+                if password_tries >= max_password_tries:
+                    return redirect('/drop')
                 errors_flag = False
             else:
                 if user.get_is_blocked():
@@ -66,6 +169,9 @@ def log_in():
                     errors_flag = False
 
         if errors_flag:
+            password_tries = 0
+            if userList.get_user_by_login(login_form).get_password() == "":
+                return redirect(f"/set_password?user={login_form}")
             if login_form == "ADMIN":
                 return redirect("/admin")
             else:
@@ -92,6 +198,83 @@ def log_in():
                             <input type="password" name="password_form" id="password_form" class="form-control"  placeholder="Введите пароль">
                             <br>
                             <input type="submit" class="btn btn-success" value="Войти">
+                            <br>
+                            <br>
+                            <a class="btn btn-danger" href="/drop">Завершить работу</a>
+                            <br>
+                        </form>
+                    </div>
+                </body>
+            </html>"""
+
+
+@app.route("/set_password", methods=["POST", "GET"])
+def set_password():
+    change_errors = []
+    arg_user = request.args.get("user")
+    if request.method == "POST":
+        if arg_user:
+            errors_flag = True
+            user = userList.get_user_by_login(arg_user)
+            new_password_form = request.form['new_password_form']
+            second_new_password_form = request.form['second_new_password_form']
+
+            if new_password_form != second_new_password_form:
+                change_errors.append("Пароли не совпадают")
+                errors_flag = False
+            if user.get_is_password_limited():
+                if not re.fullmatch(pattern, new_password_form):
+                    change_errors.append(
+                        "Пароль не удовлетворяет условию: Чередование цифр, знаков препинания и снова цифр.")
+                    errors_flag = False
+            if user.get_min_password_len() != 0:
+                if len(new_password_form) < user.get_min_password_len():
+                    change_errors.append(
+                        "Пароль слишком короткий")
+                    errors_flag = False
+
+            if errors_flag:
+                user.set_password(new_password_form)
+                if user.get_login() == "ADMIN":
+                    return redirect("/admin")
+                else:
+                    return redirect(f"/user?user={arg_user}")
+
+    error_message = ""
+    if len(change_errors) != 0:
+        error_message = '<div class="alert alert-danger" role="alert"><ul class="errors">'
+        for i in change_errors:
+            error_message += f"<li>{i}</li>"
+        error_message += "</ul></div>"
+
+    return f"""<!DOCTYPE html>
+            <html>
+                {head_html("Изменить пароль")}
+                <body>
+                    <div class="container mt-5">
+                        <h1>Задать пароль</h1>
+                        <br>
+                        <form method="POST">
+                            <h5>Ваш пароль пуст, задайте новый (можно пропустить).</h5>
+                            <br>
+                            {error_message}
+                            <h4>Новый пароль</h4>
+                            <input type="password" name="new_password_form" id="new_password_form" class="form-control" placeholder="Новый пароль">
+                            <br>
+                            <h4>Повторите новый пароль</h4>
+                            <input type="password" name="second_new_password_form" id="second_new_password_form" class="form-control" placeholder="Повторите новый пароль">
+                            <br>
+                            <input type="submit" class="btn btn-success" value="Задать">
+                            <br>
+                            <br>
+                            {
+                            '<a class="btn btn-primary" href="/admin">Пропустить</a>'
+                            if arg_user == "ADMIN" else
+                            f'<a class="btn btn-primary" href="/user?user={arg_user}">Пропустить</a>'
+                            }
+                            <br>
+                            <br>
+                            <a class="btn btn-danger" href="/drop">Завершить работу</a>
                             <br>
                         </form>
                     </div>
@@ -122,6 +305,11 @@ def change_password():
                 if not re.fullmatch(pattern, new_password_form):
                     change_errors.append(
                         "Пароль не удовлетворяет условию: Чередование цифр, знаков препинания и снова цифр.")
+                    errors_flag = False
+            if user.get_min_password_len() != 0:
+                if len(new_password_form) < user.get_min_password_len():
+                    change_errors.append(
+                        "Пароль слишком короткий")
                     errors_flag = False
 
             if errors_flag:
@@ -180,6 +368,13 @@ def user():
                         <a class="btn btn-primary" href="/change_password?user={arg_user}">Сменить пароль</a>
                         <br>
                         <br>
+                        <a class="btn btn-primary" href="/exit">Выйти</a>
+                        <br>
+                        <br>
+                        <a class="btn btn-warning" href="/about">Справка</a>
+                        <br>
+                        <br>
+                        <a class="btn btn-danger" href="/drop">Завершить работу</a>
                         <br>
                         <br>
                         <br>
@@ -231,6 +426,11 @@ def edit():
                 edit_errors.append("Пароль не удовлетворяет условию: Чередование цифр, знаков препинания и снова цифр.")
                 error_flag = False
 
+        if min_password_len_form != 0:
+            if len(password_form) < int(min_password_len_form):
+                edit_errors.append("Пароль слишком короткий")
+                error_flag = False
+
         if error_flag:
             userList.add_user(User.new_user(login_form, email_form, password_form, False, is_password_limited_switch, int(min_password_len_form), int(password_time_form)))
             return redirect("/admin")
@@ -257,12 +457,6 @@ def edit():
                                 <h4>Почта</h4>
                                 <input type="text" name="email_form" id="email_form" class="form-control" value="{email_form}">
                                 <br>
-                                <h4>Пароль</h4>
-                                <input type="password" name="password_form" id="password_form" class="form-control" value="{password_form}">
-                                <br>
-                                <h4>Повторите пароль</h4>
-                                <input type="password" name="second_password_form" id="second_password_form" class="form-control" value="{second_password_form}">
-                                <br>
                                 <div class="form-check form-switch">
                                     <input class="form-check-input" type="checkbox" role="switch" id="is_password_limited_switch" name="is_password_limited_switch" {"checked" if is_password_limited_switch else ""}>
                                     <label class="form-check-label" for="is_password_limited_switch">Ограничения пароля (Чередование цифр, знаков препинания и снова цифр)</label>
@@ -277,6 +471,12 @@ def edit():
                                     <input type="number" id="password_time_form" name="password_time_form" value="{password_time_form}" min="0" class="form-control"/>
                                 </div>
                                 <br>
+                                <h4>Пароль</h4>
+                                <input type="password" name="password_form" id="password_form" class="form-control" value="{password_form}">
+                                <br>
+                                <h4>Повторите пароль</h4>
+                                <input type="password" name="second_password_form" id="second_password_form" class="form-control" value="{second_password_form}">
+                                <br>
                                 <input type="submit" class="btn btn-success" value="Сохранить">
                                 <br>
                                 <br>
@@ -288,28 +488,6 @@ def edit():
                         </div>
                     </body>
                 </html>"""
-
-
-@app.route("/change_is_password_limited")
-def change_is_password_limited():
-    user_login = request.args.get("user")
-    if user_login:
-        user = userList.get_user_by_login(user_login)
-        if user:
-            user.set_is_password_limited(not user.get_is_password_limited())
-
-    return redirect("/admin")
-
-
-@app.route("/change_is_blocked")
-def change_is_blocked():
-    user_login = request.args.get("user")
-    if user_login:
-        user = userList.get_user_by_login(user_login)
-        if user:
-            user.set_is_blocked(not user.get_is_blocked())
-
-    return redirect("/admin")
 
 
 @app.route("/admin")
@@ -382,6 +560,15 @@ def admin():
                         <br>
                         <br>
                         <a class="btn btn-primary" href="/edit">Добавить пользователя</a>
+                        <br>
+                        <br>
+                        <a class="btn btn-primary" href="/exit">Выйти</a>
+                        <br>
+                        <br>
+                        <a class="btn btn-warning" href="/about">Справка</a>
+                        <br>
+                        <br>
+                        <a class="btn btn-danger" href="/drop">Завершить работу</a>
                         <br>
                         <br>
                         <h3>Пользователи:</h3>
